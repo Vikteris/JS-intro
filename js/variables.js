b777fd50574c985eaef53ff42e15e5da9741629d
Viktoras <vikter@inbox.lt>
2020-04-27 14:11:11 +0300

    Initial commit and variable init

---
+++ js/variables.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/* 
+kintmųjų tipai:
+
+- const (konstanta)
+- let (kintamusju tipas keičiama n kartu)
+- var (galim buti keičiamos kintamuju reiksmes)
+*/
+
+const vardas = 'Viktoras';  // tekstas
+const akiuSkaicius = 2;     // skaicius
+const turiGalva = true;     // logine reiksme true/false
+const mokaSkraidyti =false;  //logine reiksme true/false
+const zigmoPazymiai = [];         // tuscias sarasas
+const marytesPazymiai = [10, 8 ,2];    //sarasas su skaiciais
+const abc = ['a', 'b', 'd', 'e', 'f']   //sarašas su tekstai
+
+
+
+console.log( vardas );
+console.log( akiuSkaicius );
+console.log( turiGalva );
+console.log( mokaSkraidyti );
+console.log( zigmoPazymiai );
+console.log( marytesPazymiai );
+console.log( abc );
+
+
+
+let megstamasSkaicius = 5;
+console.log ( 'Mesgtamas skaicius', megstamasSkaicius);
+
+megstamasSkaicius= 3;
+console.log ( 'Mesgtamas skaicius', megstamasSkaicius);
